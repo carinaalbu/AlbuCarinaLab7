bb62d0ec9462d0fba956b70792b3804698479b26
Carina Albu <carinamariaalbu9@gmail.com>
2024-12-03 15:26:04 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace AlbuCarinaLab7
+﻿using System;
+using AlbuCarinaLab7.Data;
+using System.IO;
+
+namespace AlbuCarinaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
