3a98879277c099d299ea6f7d701ddd903844ba23
Carina Albu <carinamariaalbu9@gmail.com>
2024-12-12 14:28:20 +0200

    Lab8 cu erori

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,4 +1,4 @@
-using AlbuCarinaLab7.Models;
+﻿using AlbuCarinaLab7.Models;
 namespace AlbuCarinaLab7;
 
 public partial class ListPage : ContentPage
@@ -21,5 +21,46 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
 
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        if (listView.SelectedItem != null)
+        {
+            var selectedProduct = listView.SelectedItem as Product;
+            if (selectedProduct != null)
+            {
+                // Șterge produsul din baza de date
+                var shopList = (ShopList)this.BindingContext;
+
+                // Găsește relația între produs și lista curentă
+                var listProduct = await App.Database.GetListProductAsync(shopList.ID, selectedProduct.ID);
+                if (listProduct != null)
+                {
+                    await App.Database.DeleteListProductAsync(listProduct);
+
+                    // Actualizează afișarea ListView-ului
+                    listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+                }
+            }
+        }
+        else
+        {
+            await DisplayAlert("Error", "Please select an item to delete.", "OK");
+        }
+    }
 }
